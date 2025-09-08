46ea982fe6763c818d7f3824ab07305749c192a3
NguyenMinhTri <chinjsu130205@gmail.com>
2025-09-08 22:48:07 +0700

    add comment and gitignore

--- Script.py
+++ Script.py
@@ -1,3 +1,14 @@
 # Script run
 # python -m game_server.main
 # python -m ai_bot.main --player-id player001
+# Add --auto-load to resume from last save checkpoint in model/checkpoint
+# It will wait until other players join then auto start the game
+# This model i use the PPO algorithm so it is a little bit stupid, u can try another one to learn for ai_bot 
+# When dísconnect, it will auto save the model to model/checkpoint.
+# when disconnect, it will auto save the model to model/checkpoint.
+
+# Log du lieu grpc truoc khi gui, moi file chua 5p du lieu
+# Dong goi lai ai_bot
+# Tao room, timeout 30p, set password, num player toi da
+# ai_bot them  logic ket noi phong
+# random collision
